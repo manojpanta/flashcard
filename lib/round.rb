6db4ac75ad6ba74e0aca0bd32d73fda40b699e81
Manoj Panta <manojpanta95@gmail.com>
2018-02-13 17:09:16 -0700

    number correct modified

--- lib/round.rb
+++ lib/round.rb
@@ -2,13 +2,13 @@ require_relative 'guess'
 
 class Round
 
-attr_reader :deck, :guesses, :right_guesses, :number_correct
+attr_reader :deck, :guesses, :right_guesses
   def initialize(deck)
     @deck = deck
     @guesses = []
     @card_count = 0
     @right_guesses = []
-    @number_correct = 0
+
   end
 
   def current_card
@@ -21,15 +21,19 @@ attr_reader :deck, :guesses, :right_guesses, :number_correct
     @card_count += 1
     if result.correct?
       @right_guesses << result
-      @number_correct += 1
+
     end
 
 
     result
   end
 
+  def number_correct
+    @right_guesses.length
+  end
+
   def percent_correct
-    require 'pry' ; binding.pry
+    # require 'pry' ; binding.pry
     ((@right_guesses.count.to_f / @guesses.count.to_f) * 100).to_i
 
   end
